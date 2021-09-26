0c0e5f65d50066c960c94fc5844647c43e60acc8
Ray Kwak <91390124+Piemanray314@users.noreply.github.com>
2021-09-26 18:35:42 -0400

    TTTPanel2
    5-in-a-row win on any nxn board

--- src/TTTPanel.java
+++ src/TTTPanel.java
@@ -3,20 +3,24 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TTTPanel extends JPanel implements MouseListener {
-    final int SCREEN_DIMENSION = 600;
-    final int NUM_ROW = 5;
-    final int UNIT_SIZE = SCREEN_DIMENSION/NUM_ROW;
-    int[] TTT = new int[NUM_ROW * NUM_ROW];
+    final int SCREEN_WIDTH = 600;
+    final int UNIT_DIMENSION = 20; // Makes a n x n board
+    final int UNIT_SIZE = SCREEN_WIDTH / UNIT_DIMENSION;
+    final int GAME_UNITS = SCREEN_WIDTH * SCREEN_WIDTH / UNIT_SIZE / UNIT_SIZE;
+    final int[] x = new int[SCREEN_WIDTH / UNIT_SIZE];
+    final int[] y = new int[SCREEN_WIDTH / UNIT_SIZE];
+    int[] TTT = new int[GAME_UNITS];
     int win = 3; // 3 = game is playing, 2 = tie, 1 = Player 1 wins, 0 = Player 2 wins.
     private int round = 0;
     private boolean check = true;
 
     TTTPanel() {
         this.addMouseListener(this);
-        this.setPreferredSize(new Dimension(SCREEN_DIMENSION, SCREEN_DIMENSION));
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_WIDTH));
         this.setBackground(Color.black);
         this.setVisible(true);
         this.setFocusable(true);
+        System.out.println(TTT.length);
         for (int i = 0; i < TTT.length; i++) {
             TTT[i] = 2;
         }
@@ -32,14 +36,14 @@ public class TTTPanel extends JPanel implements MouseListener {
         g.setColor(new Color(0xFFFFFF));
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(3));
-        for (int i = 1; i <= (SCREEN_DIMENSION/UNIT_SIZE); i++) {
-            g2.drawLine(UNIT_SIZE * i, 0, UNIT_SIZE * i, SCREEN_DIMENSION);
-            g2.drawLine(0, UNIT_SIZE * i, SCREEN_DIMENSION, UNIT_SIZE * i);
+        for (int i = 1; i <= UNIT_DIMENSION; i++) {
+            g2.drawLine(UNIT_SIZE * i, 0, UNIT_SIZE * i, SCREEN_WIDTH);
+            g2.drawLine(0, UNIT_SIZE * i, SCREEN_WIDTH, UNIT_SIZE * i);
         }
-
         for (int i = 0; i < TTT.length; i++) {
             if (TTT[i] != 2) {
-                drawShape(g, ((i % NUM_ROW + 1) * 2 - 1) * (UNIT_SIZE/2), (((i / NUM_ROW) + 1) * 2 - 1) * (UNIT_SIZE/2), TTT[i]);
+                drawShape(g, ((i % UNIT_DIMENSION + 1) * 2 - 1) * UNIT_SIZE / 2,
+                        (((i / UNIT_DIMENSION) + 1) * 2 - 1) * UNIT_SIZE / 2, TTT[i]);
             }
         }
         winScreen(g);
@@ -78,25 +82,51 @@ public class TTTPanel extends JPanel implements MouseListener {
     }
 
     public void checkDub() {
-        for (int i = 0; i < NUM_ROW; i++) {
-            for(int j = 0; j < NUM_ROW-2; j++){
-                if (TTT[(NUM_ROW)*i+j] == TTT[(NUM_ROW)*i+j+1] && TTT[(NUM_ROW)*i+j+1] == TTT[(NUM_ROW)*i+j+2] && (TTT[(NUM_ROW)*i+j] != 2)) {
-                    win = TTT[(NUM_ROW)*i+j];
-                } 
-                else if (TTT[(NUM_ROW)*j+i] == TTT[(NUM_ROW)*(j+1)+i] && TTT[(NUM_ROW)*(j+1)+i] == TTT[(NUM_ROW)*(j+2)+i] && TTT[(NUM_ROW)*j+i]!= 2) {
-                    win = TTT[(NUM_ROW)*j+i];
+        int index;
+        for (int i = 0; i < UNIT_DIMENSION; i++) {
+            for (int j = 2; j < UNIT_DIMENSION - 2; j++) {
+                index = UNIT_DIMENSION * i + j;
+                if(TTT[index - 2] == TTT[index - 1] &&
+                   TTT[index - 1] == TTT[index] &&
+                   TTT[index] == TTT[index + 1] &&
+                   TTT[index + 1] == TTT[index + 2] &&
+                   TTT[index] != 2){
+                    win = TTT[index];
+                    System.out.println("ROW");
                 }
-                //else if ()
             }
         }
-
-        for(int i = 1; i < NUM_ROW-1; i++){
-            for (int j=1; j< NUM_ROW-1; j++){
-                if (TTT[NUM_ROW*i+j]==TTT[NUM_ROW*(i+1)+j-1] && TTT[NUM_ROW*i+j]==TTT[NUM_ROW*(i-1)+j+1] && TTT[NUM_ROW*i+j]!=2){
-                    win = TTT[NUM_ROW*i+j];
+        for (int i = 0; i < UNIT_DIMENSION; i++) {
+            for (int j = 0; j < UNIT_DIMENSION - 4; j++) {
+                index = UNIT_DIMENSION * (2 + j) + i;
+                if(TTT[index - 2 * UNIT_DIMENSION] == TTT[index - 1 * UNIT_DIMENSION] &&
+                   TTT[index - 1 * UNIT_DIMENSION] == TTT[index] &&
+                   TTT[index] == TTT[index + 1 * UNIT_DIMENSION] &&
+                   TTT[index + 1 * UNIT_DIMENSION] == TTT[index + 2 * UNIT_DIMENSION] &&
+                   TTT[index] != 2){
+                    win = TTT[index];
+                    System.out.println("COLUMN");
                 }
-                if (TTT[NUM_ROW*i+j]==TTT[NUM_ROW*(i-1)+j-1] && TTT[NUM_ROW*i+j]==TTT[NUM_ROW*(i+1)+j+1] && TTT[NUM_ROW*i+j]!=2){
-                    win = TTT[NUM_ROW*i+j];
+            }
+        }
+        for (int i = 0; i < UNIT_DIMENSION - 4; i++) {
+            for (int j = 2; j < UNIT_DIMENSION - 2; j++) {
+                index = UNIT_DIMENSION * 2 + UNIT_DIMENSION * i + j;
+                if (TTT[index] == TTT[index - UNIT_DIMENSION - 1] && 
+                    TTT[index] == TTT[index - 2 * UNIT_DIMENSION - 2] && 
+                    TTT[index] == TTT[index + UNIT_DIMENSION + 1] && 
+                    TTT[index] == TTT[index + 2 * UNIT_DIMENSION + 2] &&
+                    TTT[index] != 2) {
+                    win = TTT[index];
+                    System.out.println("DIAGONAL");
+                }
+                if (TTT[index] == TTT[index - UNIT_DIMENSION + 1] && 
+                    TTT[index] == TTT[index - 2 * UNIT_DIMENSION + 2] && 
+                    TTT[index] == TTT[index + UNIT_DIMENSION - 1] && 
+                    TTT[index] == TTT[index + 2 * UNIT_DIMENSION - 2] &&
+                    TTT[index] != 2) {
+                    win = TTT[index];
+                    System.out.println("DIAGONAL");
                 }
             }
         }
@@ -107,7 +137,7 @@ public class TTTPanel extends JPanel implements MouseListener {
         FontMetrics metric = getFontMetrics(g.getFont());
         if (check) {
             asdfasdf: if (true) {
-                for (int i = 0; i < TTT.length; i++) {
+                for (int i = 0; i < 9; i++) {
                     if (TTT[i] == 2) {
                         break asdfasdf;
                     }
@@ -115,7 +145,7 @@ public class TTTPanel extends JPanel implements MouseListener {
                 g.setColor(new Color(0xCBC3E3));
                 g.fillRect(0, 0, 600, 600);
                 g.setColor(new Color(0x301934));
-                g.drawString("Tie!", (SCREEN_DIMENSION - metric.stringWidth("Tie!")) / 2,
+                g.drawString("Tie!", (SCREEN_WIDTH - metric.stringWidth("Tie!")) / 2,
                         300 - (g.getFont().getSize() / 2));
             }
             switch (win) {
@@ -123,7 +153,7 @@ public class TTTPanel extends JPanel implements MouseListener {
                     g.setColor(new Color(0xFF7F7F));
                     g.fillRect(0, 0, 600, 600);
                     g.setColor(new Color(0x530000));
-                    g.drawString("Player 1 Wins!", (SCREEN_DIMENSION - metric.stringWidth("Player 1 Wins!")) / 2,
+                    g.drawString("Player 1 Wins!", (SCREEN_WIDTH - metric.stringWidth("Player 1 Wins!")) / 2,
                             300 - (g.getFont().getSize() / 2));
                     check = false;
                     break;
@@ -131,21 +161,22 @@ public class TTTPanel extends JPanel implements MouseListener {
                     g.setColor(new Color(0xADD8E6));
                     g.fillRect(0, 0, 600, 600);
                     g.setColor(new Color(0x00008B));
-                    g.drawString("Player 2 Wins!", (SCREEN_DIMENSION - metric.stringWidth("Player 2 Wins!")) / 2,
+                    g.drawString("Player 2 Wins!", (SCREEN_WIDTH - metric.stringWidth("Player 2 Wins!")) / 2,
                             300 - (g.getFont().getSize() / 2));
 
                     check = false;
                     break;
             }
-            
+
         }
     }
+
     @Override
     public void mouseClicked(MouseEvent e) {
         if (check) {
             int x = (int) (e.getX() / UNIT_SIZE);
             int y = (int) (e.getY() / UNIT_SIZE);
-            int index = x + NUM_ROW * y;
+            int index = x + UNIT_DIMENSION * y;
 
             if (TTT[index] == 2) {
                 System.out.println("Player " + (round + 1) + ": Cell" + index);
@@ -153,95 +184,13 @@ public class TTTPanel extends JPanel implements MouseListener {
                 turn();
             }
 
+            System.out.println(TTT[index]);
+
             repaint();
             checkDub();
             System.out.println("Win: " + win);
         }
     }
-    /*@Override
-    public void mouseClicked(MouseEvent e) {
-        if (check) {
-            int x = (int) (e.getX() / UNIT_SIZE);
-            int y = (int) (e.getY() / UNIT_SIZE);
-
-            // int m = (int)((x - 1) / UNIT_SIZE) * 10 + (int)((y - 1) / UNIT_SIZE);
-            String coords = "";
-            for(int i = 0; i < TTT.length; i++){
-                coords += (i % NUM_ROW + 1);
-                coords += (i / NUM_ROW + 1);
-            }
-            // 3x3 Coords String coords = "112131122232132333";
-
-            Boolean doTurn = true;
-            big:for (int i = 0; i < TTT.length; i++) {
-                if (x < Integer.parseInt(coords.substring(i * 2, i * 2 + 1))
-                        && y < Integer.parseInt(coords.substring(i * 2 + 1, i * 2 + 2))) {
-                    if (TTT[i] != 2){
-                        doTurn = false;
-                        break big;
-                    }
-                    System.out.println("Player " + (round + 1) + ": Cell" + i);
-                    TTT[i] = round;
-                    break big;
-                }
-            }
-            if(doTurn){
-                turn();
-            }
-            asdf: if (true) {
-                if (x < 1 && y < 1) {
-                    if (TTT[0] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 0");
-                    TTT[0] = round;
-                } else if (x < 2 && y < 1) {
-                    if (TTT[1] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 1");
-                    TTT[1] = round;
-                } else if (x < 3 && y < 1) {
-                    if (TTT[2] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 2");
-                    TTT[2] = round;
-                } else if (x < 1 && y < 2) {
-                    if (TTT[3] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 3");
-                    TTT[3] = round;
-                } else if (x < 2 && y < 2) {
-                    if (TTT[4] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 4");
-                    TTT[4] = round;
-                } else if (x < 3 && y < 2) {
-                    if (TTT[5] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 5");
-                    TTT[5] = round;
-                } else if (x < 1 && y < 3) {
-                    if (TTT[6] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 6");
-                    TTT[6] = round;
-                } else if (x < 2 && y < 3) {
-                    if (TTT[7] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 7");
-                    TTT[7] = round;
-                } else if (x < 3 && y < 3) {
-                    if (TTT[8] != 2)
-                        break asdf;
-                    System.out.println("Player " + (round + 1) + ": Cell 8");
-                    TTT[8] = round;
-                }
-                turn();
-            }
-            repaint();
-            checkDub();
-            System.out.println("Win: " + win);
-        }
-    }*/
 
     @Override
     public void mousePressed(MouseEvent e) {
